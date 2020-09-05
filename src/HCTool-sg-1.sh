709c24012ffd43cf73dc400c6dc9bb103576c710
Jay <chenjian_sun@hotmail.com>
2020-09-05 18:50:48 +0800

    Update HCTool-sg-1.sh

--- src/HCTool-sg-1.sh
+++ src/HCTool-sg-1.sh
@@ -6,17 +6,21 @@ if [ $# == 0 ];then
 elif [ $# == 2 ];then
     if [ -f "$1" ];then
         if [ 0"$EN_CRED_JSON_STR" = "0" ];then
-	    export EN_CRED_JSON_STR="$(cat $1)"
-	else
-	    echo "cred env is exist."
-	fi
+            export EN_CRED_JSON_STR="$(cat $1)"
+        else
+            echo "cred env is exist."
+        fi
         python ./HCTool-sg-1.py -k $2
     else
         echo "cred file is not exist!"
     fi
 elif [ $# == 3 ];then
     if [ -f "$1" ];then
-	export EN_CRED_JSON_STR="$(cat $1)"
+        if [ 0"$EN_CRED_JSON_STR" = "0" ];then
+            export EN_CRED_JSON_STR="$(cat $1)"
+        else
+            echo "cred env is exist."
+        fi
         python ./HCTool-sg-1.py -k $2 -i $3
     else
         echo "cred file is not exist!"
