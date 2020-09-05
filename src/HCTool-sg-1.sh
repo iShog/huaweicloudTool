b5c6f40f9c6b573dcaec49263b79970b4859f2a6
iShog <chenjian_sun@hotmail.com>
2020-09-05 18:41:48 +0800

    Finish the script for Termux.

--- src/HCTool-sg-1.sh
+++ src/HCTool-sg-1.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-export EN_CRED_JSON_STR="$(cat $1)"
-##echo $EN_CRED_JSON_STR
-
-python ./HCTool-sg-1.py -k $2
+if [ $# == 0 ];then
+    echo "useage xxx.sh [cred file] [key] (ip addr)"
+    echo "(ip addr) is optional."
+elif [ $# == 2 ];then
+    if [ -f "$1" ];then
+        if [ 0"$EN_CRED_JSON_STR" = "0" ];then
+	    export EN_CRED_JSON_STR="$(cat $1)"
+	else
+	    echo "cred env is exist."
+	fi
+        python ./HCTool-sg-1.py -k $2
+    else
+        echo "cred file is not exist!"
+    fi
+elif [ $# == 3 ];then
+    if [ -f "$1" ];then
+	export EN_CRED_JSON_STR="$(cat $1)"
+        python ./HCTool-sg-1.py -k $2 -i $3
+    else
+        echo "cred file is not exist!"
+    fi
+else
+    echo "invaild parameters!"
+    echo "useage xxx.sh [cred file] [key] (ip addr)\n"
+    echo "(ip addr) is optional."
+fi
